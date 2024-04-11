631ecfd3e1c69fa654e409acfff00ab6717766ea
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2024-04-11 17:11:26 -0400

    Added privacy manifest

--- Package.swift
+++ Package.swift
@@ -26,7 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ShapeUp",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("PrivacyInfo.xcprivacy")]),
         .testTarget(
             name: "ShapeUpTests",
             dependencies: ["ShapeUp"]),
